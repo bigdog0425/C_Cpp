e61b6d1b5116df3a1c64c09e74c4f3632280d569
海青大狗哥 <39252443+bigdog0425@users.noreply.github.com>
2019-03-07 11:30:18 +0800

    Update 洗牌.cpp

--- 洗牌.cpp
+++ 洗牌.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cstdlib> /* 亂數相關函數 */
 #include <ctime>   /* 時間相關函數 */
-used namespace std;
+using namespace std;
 
 int main(){
   /* 固定亂數種子 */
   srand( time(NULL) );
   int n[53],temp;
-  for (int i=1;i<53;i++) n[i]=i;/*整理為預設排組 */
+  int max=52; //最大數字
+  int min=1; //最小數字
+  for (int i=1;i<=max;i++) n[i]=i;//整理為預設排組
   /* 洗牌 */
-  for (int i=1;i<=52;i++) {
-    int x = rand()%(53-i)+i;
+  for (int i=1;i<=max;i++) {
+    int x = rand()%(max+1-i)+i;
     temp=n[i];n[i]=n[x];n[x]=temp;
     cout<<"n["<<i<<"]="<<n[i]<<std::endl;
   }
