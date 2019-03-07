4f63031da9ddfff5f3269a3bda71f7f11c1893a0
海青大狗哥 <39252443+bigdog0425@users.noreply.github.com>
2019-03-07 11:32:52 +0800

    Update 洗牌.cpp

--- 洗牌.cpp
+++ 洗牌.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int main(){
-  /* 固定亂數種子 */
-  srand( time(NULL) );
+  srand( time(NULL) ); //隨機亂數種子
   int n[53],temp;
   int max=52; //最大數字
   int min=1; //最小數字
   for (int i=1;i<=max;i++) n[i]=i;//整理為預設排組
   /* 洗牌 */
   for (int i=1;i<=max;i++) {
-    int x = rand()%(max+1-i)+i;
+    int x = rand()%(max+1-i)+i; //產生 [min,max] 的整數亂數
     temp=n[i];n[i]=n[x];n[x]=temp;
     cout<<"n["<<i<<"]="<<n[i]<<std::endl;
   }
