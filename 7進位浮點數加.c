e698b385bf7e3fe64932cb843ddef4284d2d2c96
海青大狗哥 <39252443+bigdog0425@users.noreply.github.com>
2019-03-21 11:05:05 +0800

    Update 7進位浮點數加.c

--- 7進位浮點數加.c
+++ 7進位浮點數加.c
@@ -8,10 +8,7 @@ int main ()
 	//while 1 {
 	printf("請輸入兩個7進位的數字：");
 	scanf("%Lf%Lf",&a1,&b1);
-	
-	//if () printf("\n輸入格式正確\n") else break;
-	//}
-		
+/* 將浮點數a1,b1轉為長整數a2,b2並記錄小數位數於c */	
 	for(;(long long int)a1<a1||(long long int)b1<b1;c=c*0.1)
 	{
 		a1=a1*10;
@@ -19,6 +16,8 @@ int main ()
 	}
 	a2=(long long int)a1;
 	b2=(long long int)b1;
+
+	/* 將十進位轉為七進位 */
 	a1=0;
 	while(a2>0||b2>0)
 	{
@@ -27,10 +26,6 @@ int main ()
 	}
 	sprintf(A,"%f",a1);
 	b2=0;
-	for(a2=14;a2>=0;a2=a2-1)
-	{
-		
-	}
 	printf("%s",A);
 	return 0;
 }
