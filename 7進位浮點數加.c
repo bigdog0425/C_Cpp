73e314667e9355f4cb4bbe4583f4dbba750d0afa
海青大狗哥 <39252443+bigdog0425@users.noreply.github.com>
2019-03-21 10:41:38 +0800

    Create 7進位浮點數加.c

---
+++ 7進位浮點數加.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main () 
+{
+	double a1=0,b1=0,c=1;
+	long long int a2=0,b2=0;
+	char zero = '1';
+	char A[15]={'\0'};
+	//while 1 {
+	printf("請輸入兩個7進位的數字：");
+	scanf("%Lf%Lf",&a1,&b1);
+	
+	//if () printf("\n輸入格式正確\n") else break;
+	//}
+		
+	for(;(long long int)a1<a1||(long long int)b1<b1;c=c*0.1)
+	{
+		a1=a1*10;
+		b1=b1*10;
+	}
+	a2=(long long int)a1;
+	b2=(long long int)b1;
+	a1=0;
+	while(a2>0||b2>0)
+	{
+		a1=a1+(double)((a2%10+b2%10)%7)*c;c=c*10;
+		a2=(a2/10)+((a2%10+b2%10)/7);b2=(b2/10);	
+	}
+	sprintf(A,"%f",a1);
+	b2=0;
+	for(a2=14;a2>=0;a2=a2-1)
+	{
+		
+	}
+	printf("%s",A);
+	return 0;
+}
